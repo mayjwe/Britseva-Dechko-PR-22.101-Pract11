dfd743b0e7dbfbcefaac9fc00e2b50ab68e63408
Mihail <20.08.2005darkness@gmail.com>
2024-05-30 11:09:14 +0700

    all done

--- app/src/main/java/com/example/practicheskaya11teslukinikolaenko/Map.java
+++ app/src/main/java/com/example/practicheskaya11teslukinikolaenko/Map.java
@@ -1,35 +1,28 @@
 package com.example.practicheskaya11teslukinikolaenko;
 
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.R.id;
-
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.practicheskaya11teslukinikolaenko.MESSAGE";
+public class Map extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-    }
-
-    public void onClickStepsActivity(View view) {
-        Intent intent = new Intent(this, steps.class);
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
         startActivity(intent);
     }
-
 }
--- app/src/main/java/com/example/practicheskaya11teslukinikolaenko/steps.java
+++ app/src/main/java/com/example/practicheskaya11teslukinikolaenko/steps.java
@@ -1,6 +1,8 @@
 package com.example.practicheskaya11teslukinikolaenko;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class steps extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClickStepsActivity(View view) {
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
     }
 }
